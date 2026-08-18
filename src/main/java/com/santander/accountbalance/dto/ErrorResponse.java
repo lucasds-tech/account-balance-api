fd75333bec1dc81329310c3c051d858da3e9f803
lucasds-tech <lucas.dasilva09@hotmail.com>
2026-08-18 10:32:07 -0300

    feat: add error response DTO

---
+++ src/main/java/com/santander/accountbalance/dto/ErrorResponse.java
@@ -0,0 +1,10 @@
+package com.santander.accountbalance.dto;
+
+import java.time.Instant;
+
+public record ErrorResponse(Instant timestamp,
+                            int status,
+                            String error,
+                            String message,
+                            String path) {
+}
