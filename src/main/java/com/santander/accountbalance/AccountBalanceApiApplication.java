160e5621b34d040cf67f74e8780283bb04a459fc
lucasds-tech <lucas.dasilva09@hotmail.com>
2026-08-18 08:58:00 -0300

    initial commit project

---
+++ src/main/java/com/santander/accountbalance/AccountBalanceApiApplication.java
@@ -0,0 +1,13 @@
+package com.santander.accountbalance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountBalanceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountBalanceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/santander/accountbalance/AccountBalanceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santander.accountbalance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountBalanceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
