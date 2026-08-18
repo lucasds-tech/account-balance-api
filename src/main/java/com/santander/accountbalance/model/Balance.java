390f4d6c166fd76c8abd9b44f3b077d60e09c220
lucasds-tech <lucas.dasilva09@hotmail.com>
2026-08-18 09:26:36 -0300

    refactor: adjust Lombok annotations

--- src/main/java/com/santander/accountbalance/model/Balance.java
+++ src/main/java/com/santander/accountbalance/model/Balance.java
@@ -4,15 +4,14 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.Instant;
 import java.util.UUID;
 
-@Data
+@Getter
+@Setter
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
