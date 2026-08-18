aaaa399f7a7e162dc67899f8aad3fd376e5ba98a
lucasds-tech <lucas.dasilva09@hotmail.com>
2026-08-18 09:18:03 -0300

    feat: define account repository

---
+++ src/main/java/com/santander/accountbalance/repository/AccountRepository.java
@@ -0,0 +1,9 @@
+package com.santander.accountbalance.repository;
+
+import com.santander.accountbalance.model.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface AccountRepository extends JpaRepository<Account, UUID> {
+}
