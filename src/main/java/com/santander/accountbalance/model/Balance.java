9c24bbd5e35d5232daf2edd2a6a2cffe53c87ed6
lucasds-tech <lucas.dasilva09@hotmail.com>
2026-08-18 09:23:34 -0300

    feat: add balance entity fields

---
+++ src/main/java/com/santander/accountbalance/model/Balance.java
@@ -0,0 +1,36 @@
+package com.santander.accountbalance.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.UUID;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "balances")
+
+public class Balance {
+
+    @Id
+    @Column(name = "account_id")
+    private UUID accountId;
+
+    @Column(nullable = false, scale = 2)
+    private BigDecimal amount;
+
+    @Column(nullable = false, length = 3)
+    private String currency;
+
+    @Column(name = "updated_at", nullable = false)
+    private Instant updatedAt;
+
+}
